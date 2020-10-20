f75a70d99444189d1b3723e4f6b819c1824a79d3
nlnadialia <nlnadialigia@gmail.com>
2020-10-19 21:55:01 -0300

    status(404)

--- modulo-03/server.ts
+++ modulo-03/server.ts
@@ -3,14 +3,29 @@ import nunjucks from 'nunjucks'
 
 const server = express()
 
-server.set('view engine', 'html')
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
+    
 })
 
 server.get('/', function (request, response) {
-    return response.render('index');
+    return response.render('about');
 })
+
+server.get('/portfolio', function (request, response) {
+    return response.render('portfolio')
+})
+
+server.get('/not-found', function(request, response) {
+    return response.status(404).render('not-found')
+})
+
+
 
 server.listen(5000, () => {console.log('SERVER IS RUNNING')})
 
